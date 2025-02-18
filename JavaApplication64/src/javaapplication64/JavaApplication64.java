6d2bdce0ae5aca1fbcac439eedd0906a048ba26d
dpsumanth <sumanth.doijode123@gmail.com>
2025-02-18 15:15:49 +0530

    java

---
+++ JavaApplication64/src/javaapplication64/JavaApplication64.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication64;
+
+/**
+ *
+ * @author ise
+ */
+public class JavaApplication64 {
+    public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Hello, World!");
+    }
+}
